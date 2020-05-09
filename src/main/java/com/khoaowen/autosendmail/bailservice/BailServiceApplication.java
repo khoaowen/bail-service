d42e0a87384cc467b5228fe657a567c9f3c36230
khoaowen <khoaowen>
2020-05-09 10:57:38 +0200

    Initial commit

---
+++ src/main/java/com/khoaowen/autosendmail/bailservice/BailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.khoaowen.autosendmail.bailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BailServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BailServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khoaowen/autosendmail/bailservice/BailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khoaowen.autosendmail.bailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
